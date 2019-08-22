d0638e6187771a7351f79b574aefab838c5245a8
hellomrbigshot <1994yechen@gmail.com>
2019-08-22 17:19:14 +0800

    update

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
-
 import MEditor from 'react-m-editor'
+import logo from './logo.svg'
+
 // import { text } from './example'
 
 export default class App extends Component {
   render () {
     return (
-      <div style={{margin: '50px auto', width: '90%'}}>
-        <MEditor value='hello world'/>
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <div style={{margin: '20px auto', width: '90%'}}>
+          <MEditor value='hello world'/>
+        </div>
       </div>
     )
   }
